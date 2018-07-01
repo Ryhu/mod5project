8c732e379db4d8dca7d4492390e8ec29578cdc28
Ryan Hu <ryanhuuu@gmail.com>
2018-07-01 19:36:14 -0400

    fixed issue where adding to cart would create doubles- now it stacks

--- cuisineComposerApi/app/controllers/api/v1/shopping_cart_ingredients_controller.rb
+++ cuisineComposerApi/app/controllers/api/v1/shopping_cart_ingredients_controller.rb
@@ -1,12 +1,21 @@
 class Api::V1::ShoppingCartIngredientsController < ApplicationController
 
   def create
-    @shopping_cart_ingredient = ShoppingCartIngredient.new(shopping_cart_ingredient_params)
-    if @shopping_cart_ingredient.save
-      render json: {message: "saved!"}
+    @shopping_cart_ingredient = ShoppingCartIngredient.find_by(ingredient_id: shopping_cart_ingredient_params[:ingredient_id])
+    if !@shopping_cart_ingredient
+      @shopping_cart_ingredient = ShoppingCartIngredient.new(shopping_cart_ingredient_params)
+      if @shopping_cart_ingredient.save
+        render json: {message: "created!"}
+      else
+        render json: {error: "Something went wrong!"}
+      end
     else
-      render json: {error: "Something went wrong!"}
+
+      newAmount = @shopping_cart_ingredient.amount + shopping_cart_ingredient_params[:amount]
+      @shopping_cart_ingredient.update(amount: newAmount)
+      render json: {message: "ingredient stacked!"}
     end
+
   end
 
 
